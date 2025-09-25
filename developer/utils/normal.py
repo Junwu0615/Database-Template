42be063323f622853e2ebb91edcb0f81da6b677a
jun.audis5 <jun.audis5@gmail.com>
2025-09-26 00:31:24 +0800

    update somethings

--- developer/utils/normal.py
+++ developer/utils/normal.py
@@ -103,9 +103,22 @@ def trans_replace(target: str, symbol_list: list) -> str:
     return target
 
 
-def get_datetime_now(tz: timezone=TZ_UTC_8) -> datetime:
-    # 轉台灣時間 UTC +8
-    return datetime.now().replace(tzinfo=tz)
+def get_now(hours: int=None, minutes: int=None, seconds: int=None, tzinfo: timezone=None) -> datetime:
+    target_time = datetime.utcnow()
+
+    if hours is not None:
+        target_time += timedelta(hours=hours)
+
+    if minutes is not None:
+        target_time += timedelta(minutes=minutes)
+
+    if seconds is not None:
+        target_time += timedelta(seconds=seconds)
+
+    if tzinfo is not None:
+        target_time = target_time.replace(tzinfo=tzinfo)
+
+    return target_time
 
 
 def trans_datetime(target: str, date_format: str, tz: timezone=TZ_UTC_8) -> datetime:
