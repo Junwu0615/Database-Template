25377e0706460deeaab00ca45226d2b5240ba82f
jun.audis5 <jun.audis5@gmail.com>
2025-09-26 00:17:53 +0800

    update somethings

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='developer',
     version='0.1.0',
     author='PC',
     description='Simple underlying object logic.',
+    packages=find_packages(),
+    install_requires=required,
 )
